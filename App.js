7969e323be803889b14a58d8732fad3cb5f5e849
junaid <mac@junaids-MacBook-Air.local>
2024-12-16 17:33:15 +0530

    nested structure  using React.createElement

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 
-const heading = React.createElement('h1',{id:'heading'},'hello world')
+// how to create  nested structure  using React.createElement
 
-console.log(heading)
+// <div id="parent">
+//     <div id="child">
+//         <h1>hello world</h1>
+//         <h1>hello world</h1>
+//         <h1>hello world</h1>
+//     </div>
+//     <div id="child">
+//         <h1>hello world</h1>
+//         <h1>hello world</h1>
+//         <h1>hello world</h1>
+//     </div>
+// </div>
+
+
+const parent = React.createElement('div', { id: 'parent' }, [
+    React.createElement(
+        'div', { id: 'child' },
+        [React.createElement('h1', { id: 'heading' }, 'hello world'), React.createElement('h2', { id: 'heading' }, 'hello world')]
+    ),
+    React.createElement(
+        'div', { id: 'child2' },
+        [React.createElement('h1', { id: 'heading' }, 'hello world'), React.createElement('h2', { id: 'heading' }, 'hello world')]
+    )
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
+root.render(parent)
